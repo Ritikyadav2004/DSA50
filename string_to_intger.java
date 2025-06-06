a98052a7ec35b68a82583f4b48765770656c3847
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-06-06 21:39:51 +0530

    Day 8

---
+++ string_to_intger.java
@@ -0,0 +1,22 @@
+public class string_to_intger{
+
+    public static int converter(String str)
+    {   
+        int num  = 0; 
+        for(int i=0;i<str.length();i++)
+        {
+            if( (int)str.charAt(i)>=48 && (int)str.charAt(i)<=57)
+            {
+                // Add your logic here
+                num=num*10;
+                num = num+((int)str.charAt(i)-48);
+            }
+        }
+        return num; // Add appropriate return value
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(converter("hi there 123"));
+    }
+    
+}
